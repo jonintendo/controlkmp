f1e3add6e5ad46f37e3620d863165f82748f0177
jonathan <jonintendox@gmail.com>
2026-05-11 21:13:28 -0300

    icones de wifi

--- control/src/commonMain/kotlin/com/joystick/Icons.kt
+++ control/src/commonMain/kotlin/com/joystick/Icons.kt
@@ -7,12 +7,14 @@ import com.jonintendo.control.generated.resources.map
 import com.jonintendo.control.generated.resources.play
 import com.jonintendo.control.generated.resources.split
 import com.jonintendo.control.generated.resources.stop
+import com.jonintendo.control.generated.resources.wifi_off
+import com.jonintendo.control.generated.resources.wifi_on
 
 val mapIcon = Res.drawable.map
 val camIcon = Res.drawable.cam
-
 val splitIcon = Res.drawable.split
-
 val playIcon = Res.drawable.play
-val stopIcon =Res.drawable.stop
-val irIcon=Res.drawable.ir
+val stopIcon = Res.drawable.stop
+val irIcon = Res.drawable.ir
+val wifiOnIcon = Res.drawable.wifi_on
+val wifiOffIcon = Res.drawable.wifi_off
